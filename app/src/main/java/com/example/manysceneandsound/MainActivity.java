5bb0ea868ab60faca06cb7ec22f2515a82870ade
Wedlec129 <boryasokolov@gmail.com>
2023-02-12 15:43:35 +0300

    add:like args/argv param in screan

--- app/src/main/java/com/example/manysceneandsound/MainActivity.java
+++ app/src/main/java/com/example/manysceneandsound/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +19,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickNext(View v){
 
+        textInput=findViewById(R.id.textInput);
+
+
+
+
         //как только нажали кнопку мы создали обекст перехода
         //указали где находимься сейчас и куда перейти
         //action->new bacic activity
         Intent intent =new Intent(this,second_layout.class);
+        intent.putExtra("msg",textInput.getText().toString()); //даём параметр в интент
         startActivity(intent);
 
     }
--- app/src/main/java/com/example/manysceneandsound/second_layout.java
+++ app/src/main/java/com/example/manysceneandsound/second_layout.java
@@ -5,13 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class second_layout extends AppCompatActivity {
+
+    TextView text;
+    String msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_layout);
+
+        //получаем параметр intent и задаём в текст
+        Intent intent=getIntent();
+        text=findViewById(R.id.textvievOUT);
+        msg=intent.getStringExtra("msg");
+        text.setText(msg);
+
     }
 
 
